49428d292c8d5e794937d48ca43f374231fcd1c0
Luna <lina.gali@yahoo.com>
2024-12-26 21:50:34 +0100

    adding redix sort and time complexity for all algorithms

--- main.C
+++ main.C
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+#include <time.h>
 void swap(int *a, int *b)
 {
     int x;
@@ -12,6 +13,9 @@ void swap(int *a, int *b)
 void bubbleSort(int A[], int n)
 {
     bool change = true;
+    clock_t debut,fin;
+    double tempsExecution;
+    debut=clock();
     while (change == true)
     {
         change = false;
@@ -24,11 +28,18 @@ void bubbleSort(int A[], int n)
             }
         }
     }
+    fin=clock();
+    tempsExecution= ((double) (fin - debut)) / CLOCKS_PER_SEC;
+    printf("temps d'execution :%f secondes \n",tempsExecution);
+
 }
 void bubbleSortOpt(int A[], int n)
 {
     int m = n - 1;
     bool change = true;
+    clock_t debut,fin;
+    double tempsExecution;
+    debut=clock();
     while (change)
     {
         change = false;
@@ -42,6 +53,9 @@ void bubbleSortOpt(int A[], int n)
         }
         m = m - 1;
     }
+    fin=clock();
+    tempsExecution= ((double) (fin - debut)) / CLOCKS_PER_SEC;
+    printf("temps d'execution :%f secondes \n",tempsExecution);
 }
 
 // redix sort
@@ -100,27 +114,69 @@ void sortAux(int A[], int n, int i)
         A[j] = B[j];
     }
 }
-void redixSort(int A[], int n, int k)
-{
-    for (int i = 0; i < k; i++)
-    {
+void redixSort(int A[], int n, int k) {
+    clock_t debut, fin;
+    double tempsExecution;
+
+    debut = clock(); 
+
+    for (int i = 0; i < k; i++) {
         sortAux(A, n, i);
+    }
+
+    fin = clock();
+    tempsExecution = (double)(fin - debut) / CLOCKS_PER_SEC;
+
+    printf("Temps d'execution du tri Radix: %f secondes\n", tempsExecution);
+}
+
+
+// heap sort 
+void heapify(int A[], int n, int i) {
+    int max = i;
+    int gauche = 2 * i + 1;
+    int droit = 2 * i + 2;
+    if (gauche < n && A[gauche] > A[max]) max = gauche;
+    if (droit < n && A[droit] > A[max]) max = droit;
+    if (max != i) {
+        swap(&A[i], &A[max]);
+        heapify(A, n, max);
     }
 }
 
+void heapSort(int A[], int n) {
+    clock_t debut,fin;
+    double tempsExecution;
+    debut=clock();
+    for (int i = n / 2 - 1; i >= 0; i--) heapify(A, n, i);
+    for (int i = n - 1; i > 0; i--) {
+        swap(&A[0], &A[i]);
+        heapify(A, i, 0);
+    }
+    fin=clock();
+    tempsExecution= ((double) (fin - debut)) / CLOCKS_PER_SEC;
+    printf("temps d'execution :%f secondes \n",tempsExecution);
+
+}
+
+
 int main()
 {
-    // int T[5]={5,7,1,0,15};
-    // bubbleSortOpt(T,5);
-    // for (int i = 0; i <5; i++)
-    // {
-    //     printf(" %d ",T[i]);
-    // }
-
-    int T[] = {141, 232, 45, 112, 143};
-    int n = sizeof(T) / sizeof(T[0]);
-    int k = 3;
-    redixSort(T, n, k);
+    int T[] = {
+        1254, 8965, 7412, 3698, 9514, 6325, 4587, 1236, 7896, 4563,
+        7895, 1478, 2589, 6547, 9632, 1597, 4568, 7896, 3214, 6589,
+        4521, 7896, 3214, 6587, 9854, 1236, 5478, 9632, 1478, 5896,
+        7412, 3698, 9514, 6325, 4587, 1236, 7896, 4563, 7895, 1478,
+        2589, 6547, 9632, 1597, 4568, 7896, 3214, 6589, 4521, 7896,
+        1254, 8965, 7412, 3698, 9514, 6325, 4587, 1236, 7896, 4563,
+        7895, 1478, 2589, 6547, 9632, 1597, 4568, 7896, 3214, 6589,
+        4521, 7896, 3214, 6587, 9854, 1236, 5478, 9632, 1478, 5896,
+        7412, 3698, 9514, 6325, 4587, 1236, 7896, 4563, 7895, 1478,
+        2589, 6547, 9632, 1597, 4568, 7896, 3214, 6589, 4521, 7896
+    };
+    
+    int n = sizeof(T)/sizeof(T[0]);
+    heapSort(T,n);
     for (int i = 0; i < n; i++)
     {
         printf("%d ", T[i]);
