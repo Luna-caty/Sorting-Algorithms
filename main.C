764d77497c8e1f5095201ac8c5d957642d4694f3
Luna <lina.gali@yahoo.com>
2024-12-26 20:22:29 +0100

    adding Bubble sort and Redix sort

---
+++ main.C
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+void swap(int *a, int *b)
+{
+    int x;
+    x = *a;
+    *a = *b;
+    *b = x;
+}
+void bubbleSort(int A[], int n)
+{
+    bool change = true;
+    while (change == true)
+    {
+        change = false;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (A[i] > A[i + 1])
+            {
+                swap(&A[i], &A[i + 1]);
+                change = true;
+            }
+        }
+    }
+}
+void bubbleSortOpt(int A[], int n)
+{
+    int m = n - 1;
+    bool change = true;
+    while (change)
+    {
+        change = false;
+        for (int i = 0; i < m; i++)
+        {
+            if (A[i] > A[i + 1])
+            {
+                swap(&A[i], &A[i + 1]);
+                change = true;
+            }
+        }
+        m = m - 1;
+    }
+}
+
+// redix sort
+int key(int x, int i)
+{
+    int div = pow(10, i);
+    return (x / div) % 10;
+}
+void sortAux(int A[], int n, int i)
+{
+    int count[10] = {0}; //  tableau comptage des occurences de chiffres
+    int B[n];
+    int j, k;
+
+    // on calcule l'occurence de chaque chiffre
+    for (j = 0; j < n; j++)
+    {
+        count[key(A[j], i)]++;
+    }
+    /* pour T = {141,232,45,112,143}
+    on aura dans count
+    count = {0, 1, 2, 1, 0, 1, 0, 0, 0, 0}§
+    dans ce cas on a pris le cas des unités
+    */
+
+    // on calcule les positions cumulées pour chaque chiffre
+    for (j = 1; j < 10; j++)
+    {
+
+        count[j] += count[j - 1];
+    }
+    // resultat count = {0, 1, 3, 4, 4, 5, 5, 5, 5, 5}
+
+    // on place les elements dnas l'ordre trié selon le chiffre à la position i
+
+    /*
+    A[1]=232  le chiffre des unités c'est 2 
+    on va placer 232 a la position count [2] -1 = 3 -1 => 2 dans B 
+    et decrement count [2] (donc il devient 2)
+    
+    A[3]=112 
+    le chiffre des unités est 2 on va placer 232 a la position count [2] -1 => 2-1 =1 dans B
+    et on decremente count 2 (donc il devient 1)
+    */
+    for (j = n - 1; j >= 0; j--)
+    {
+        k = count[key(A[j], i)] - 1;
+        B[k] = A[j];
+        count[key(A[j], i)]--;
+    }
+
+    // on recopie les elements dans le tableau original
+    for (j = 0; j < n; j++)
+    {
+
+        A[j] = B[j];
+    }
+}
+void redixSort(int A[], int n, int k)
+{
+    for (int i = 0; i < k; i++)
+    {
+        sortAux(A, n, i);
+    }
+}
+
+int main()
+{
+    // int T[5]={5,7,1,0,15};
+    // bubbleSortOpt(T,5);
+    // for (int i = 0; i <5; i++)
+    // {
+    //     printf(" %d ",T[i]);
+    // }
+
+    int T[] = {141, 232, 45, 112, 143};
+    int n = sizeof(T) / sizeof(T[0]);
+    int k = 3;
+    redixSort(T, n, k);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", T[i]);
+    }
+
+    return 0;
+}
